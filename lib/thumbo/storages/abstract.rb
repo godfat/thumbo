498b62ef51cb6263723cf5286e05eca09059f7b2
Lin Jen-Shin <godfat@godfat.org>
2009-05-15 15:42:36 +0800

    oops, it should be Thumbo::AbstractStorage, not AbstractStorage.

--- lib/thumbo/storages/abstract.rb
+++ lib/thumbo/storages/abstract.rb
@@ -1,31 +1,33 @@
 
-class AbstractStorage
-  def read filename
-    raise NotImplementedError
-  end
+module Thumbo
+  class AbstractStorage
+    def read filename
+      raise NotImplementedError
+    end
 
-  def write filename, blob
-    raise NotImplementedError
-  end
+    def write filename, blob
+      raise NotImplementedError
+    end
 
-  def write_file filename, file
-    raise NotImplementedError
-  end
+    def write_file filename, file
+      raise NotImplementedError
+    end
 
-  def delete filename
-    raise NotImplementedError
-  end
+    def delete filename
+      raise NotImplementedError
+    end
 
-  def paths filename
-    raise NotImplementedError
-  end
+    def paths filename
+      raise NotImplementedError
+    end
 
-  def exist? filename
-    raise NotImplementedError
-  end
+    def exist? filename
+      raise NotImplementedError
+    end
 
-  protected
-  def raise_file_not_found filename
-    raise Thumbo::FileNotFound.new(filename)
+    protected
+    def raise_file_not_found filename
+      raise Thumbo::FileNotFound.new(filename)
+    end
   end
 end
